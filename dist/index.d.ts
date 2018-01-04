63e6d80b62c36f6cc6e19a129f3686be11396481
GABRIEL DE SOUZA SILVA <GABRIEL.SI@uninter.com>
2018-01-04 17:39:30 -0200

    commit inicial

---
+++ dist/index.d.ts
@@ -0,0 +1,6 @@
+export declare class AvatarNameInitialsComponent {
+    name: string;
+    initials: string;
+    ngOnInit(): void;
+    getNameInitials(fullName: string): string;
+}
---
+++ lib/index.ts
@@ -0,0 +1,48 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: 'avatar-name',
+    template: '<span class="gavatar" attr.aria-label="{{name}}">{{initials}}</span>',
+    styles: [`
+    .gavatar    {
+        color: white;
+        background-color: #c6c699;
+        display: inline-block;
+        font-family: Arial,"Helvetica Neue",Helvetica,sans-serif;
+        font-size: 14px;
+        border-radius: 80px;
+        width: 40px;
+        height: 36px;
+        line-height: 39px;
+        margin: 15px;
+        text-align: center;
+        }  
+  `]
+})
+export class AvatarNameInitialsComponent {
+    @Input() name: string;
+    initials: string = "";
+
+    ngOnInit() {
+        this.initials = this.getNameInitials(this.name);
+    }
+
+    //Get names's initials
+    getNameInitials(fullName: string): string {
+
+        if (fullName === null) { return ""; }
+
+        let splitedName = fullName.split(" ");
+
+        //only show the first 3 initials
+        let contador = splitedName.length > 3 ? 3 : splitedName.length;
+
+        for (let i = 0; i < contador; i++) {
+            //if the name has "de" preposition, it's not considered
+            if (splitedName[i].toUpperCase() != "DE") {
+                this.initials = this.initials + splitedName[i].charAt(0).toUpperCase() + ".";
+            }
+        }
+        return this.initials;
+    }
+}
